70e9d7d015a58eac73e7f803e13e0259cc5c7134
Cc-kang <3430339201@qq.com>
2026-06-27 18:43:36 +0800

    初次上传代码

---
+++ lianxi/lianxi.cpp
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+#include <iostream>
+//#include<iomanip>
+//using std::cout;
+//using std::cin;
+using namespace std;
+int main(){
+    float a;
+    a = 10 * 1.0 / 3;
+    cout.precision(2);//把cout的输出精度
+    cout << a;
+	return 0;
+    }
+
+
+
+
+
+	
+

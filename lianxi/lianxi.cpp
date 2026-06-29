2e040ae448c67562420c87cf81c522bad51d29b6
Cc-kang <3430339201@qq.com>
2026-06-29 14:22:45 +0800

    创建 lianxi.cpp

---
+++ lianxi/lianxi.cpp
@@ -0,0 +1,37 @@
+﻿#include<stdio.h>
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main() {
+    {
+        int n, min = 1000;
+        cout << n;
+        for (int i = 0; i < n; i++) {
+            int a;
+            cin << a;
+            if (a < min) {
+                min = a;
+            }
+        }
+        cout << min;
+        return 0;
+    }
+
+    
+
+
+
+
+
+
+
+	return 0;
+} 
+	
+
+
+
+
+
+	
+
